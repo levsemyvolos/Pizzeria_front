edd05a18bea75fbe3ebe664d3869ddb265574caf
levsemyvolos <semyvoloslev@gmail.com>
2024-12-10 00:20:21 +0200

    contact page modified

--- src/pages/LocationPage.js
+++ src/pages/LocationPage.js
@@ -1,23 +1,79 @@
 // src/pages/LocationPage.js
 import React from "react";
+import { Box, Typography, Paper, Container, Grid } from "@mui/material";
+import { LocationOn, AccessTime, Phone } from "@mui/icons-material";
 
 const LocationPage = () => {
   return (
-    <div className="location-page">
-      <h1>Our Location</h1>
-      <div className="map" style={{ marginBottom: "20px" }}>
-        <iframe
-          title="Store Location"
-          width="600"
-          height="450"
-          style={{ border: 0 }}
-          loading="lazy"
-          allowFullScreen
-          src="https://maps.google.com/maps?q=new%20york&t=&z=13&ie=UTF8&iwloc=&output=embed"
-        ></iframe>
-      </div>
-      <p>Address: 123 Main St, Open from 9:00 AM to 11:00 PM</p>
-    </div>
+    <Container maxWidth="lg">
+      <Box sx={{ py: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Contacts
+        </Typography>
+
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={6}>
+            <Paper
+              elevation={3}
+              sx={{
+                p: 3,
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+              }}
+            >
+              <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+                <LocationOn color="primary" />
+                <Typography variant="h6">Address</Typography>
+              </Box>
+              <Typography>123 Main Street, New York, NY 10001</Typography>
+
+              <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+                <AccessTime color="primary" />
+                <Typography variant="h6">Working Hours</Typography>
+              </Box>
+              <Typography>
+                Monday - Friday: 9:00 AM - 10:00 PM
+                <br />
+                Saturday - Sunday: 10:00 AM - 11:00 PM
+              </Typography>
+
+              <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+                <Phone color="primary" />
+                <Typography variant="h6">Contact</Typography>
+              </Box>
+              <Typography>
+                Phone: (555) 123-4567
+                <br />
+                Email: info@pizzashop.com
+              </Typography>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <Paper
+              elevation={3}
+              sx={{
+                height: 400,
+                overflow: "hidden",
+                position: "relative",
+              }}
+            >
+              <Box
+                component="iframe"
+                title="Store Location"
+                src="https://maps.google.com/maps?q=new%20york&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                sx={{
+                  width: "100%",
+                  height: "100%",
+                  border: 0,
+                }}
+              />
+            </Paper>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   );
 };
 
